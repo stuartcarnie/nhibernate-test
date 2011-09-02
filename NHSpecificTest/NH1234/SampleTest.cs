26841dcba7d341b96e6bb662e614f3b12f43eac9
scarnie <stuart.carnie@gmail.com>
2011-09-02 12:22:40 -0700

    Initial commit

---
+++ NHSpecificTest/NH1234/SampleTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using NHibernate.Dialect;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH1234
+{
+    [TestFixture]
+    public class SampleTest : BugTestCase
+    {
+        protected override void OnSetUp()
+        {
+            base.OnSetUp();
+            using (ISession session = this.OpenSession())
+            {
+                DomainClass entity = new DomainClass();
+                entity.Id = 1;
+                entity.ByteData = new byte[] {1, 2, 3};
+                session.Save(entity);
+                session.Flush();
+            }
+        }
+
+        protected override void OnTearDown()
+        {
+            base.OnTearDown();
+            using (ISession session = this.OpenSession())
+            {
+                string hql = "from System.Object";
+                session.Delete(hql);
+                session.Flush();
+            }
+        }
+
+        protected override bool AppliesTo(NHibernate.Dialect.Dialect dialect)
+        {
+            return dialect as MsSql2005Dialect != null;
+        }
+
+        [Test]
+        public void BytePropertyShouldBeRetrievedCorrectly()
+        {
+            using (ISession session = this.OpenSession())
+            {
+                DomainClass entity = session.Get<DomainClass>(1);
+
+                Assert.AreEqual(3, entity.ByteData.Length);
+                Assert.AreEqual(1, entity.ByteData[0]);
+                Assert.AreEqual(2, entity.ByteData[1]);
+                Assert.AreEqual(3, entity.ByteData[2]);
+            }
+        }
+    }
+}
